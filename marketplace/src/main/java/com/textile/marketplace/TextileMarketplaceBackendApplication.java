55cdbfcc10ddd73aa5ea19da08166f8350f1979a
Mohd Zaid <zaidalig1234@gmail.com>
2026-02-19 00:50:28 +0530

    first

---
+++ marketplace/src/main/java/com/textile/marketplace/TextileMarketplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.textile.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextileMarketplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextileMarketplaceBackendApplication.class, args);
+	}
+
+}
---
+++ marketplace/src/test/java/com/textile/marketplace/TextileMarketplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.textile.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextileMarketplaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
